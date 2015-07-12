40a1f107730249c8f22cc1d1f83fdf053bcd23ea
LL8054 <LL8054@Hotmail.com>
2015-07-12 00:25:24 -0500

    tokenization cont'd

--- Tokenization.R
+++ Tokenization.R
@@ -2,10 +2,15 @@
 
 library(tm)
 
-setwd("~/LL/Coursera/Courses/Capstone/CapstoneProject")
-path <- "~/LL/Coursera/Courses/Capstone/Coursera-SwiftKey/final/"
+#for windows
+setwd("~/LL/Coursera/Courses/Capstone/CapstoneProject") #for windows
+path <- "~/LL/Coursera/Courses/Capstone/Coursera-SwiftKey/final/" #for windows
+
+#for osx
+setwd("~/datasciencecoursera/Courses/Capstone/capstoneproject") #for osx
+path <- "~/datasciencecoursera/Courses/Capstone/Coursera-SwiftKey/final/" #for osx
 
 enTwitter <- scan(paste(path, "en_US/en_US.twitter.txt", sep=""), what="char", quote="", skipNul=TRUE)
 
-enTwitter.block <- VectorSource(enTwitter)
-inspect(Corpus(enTwitter.block))
+enTwitter.block.head <- VectorSource(head(enTwitter, n=1000))
+inspect(Corpus(enTwitter.block.head))
